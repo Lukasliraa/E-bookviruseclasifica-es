3c501b86fad426c26f511b249e8438da2ea9ddc3
LukasLira <143535166+Lukasliraa@users.noreply.github.com>
2024-11-29 23:33:45 -0300

    Update tela3.lua

--- tela3.lua
+++ tela3.lua
@@ -1,8 +1,13 @@
 local composer = require( "composer" )
  
 local scene = composer.newScene()
+local audio = require("audio") -- Módulo de áudio do Corona
+
  
 local MARGIN = 90
+local somAtivo -- Variável para armazenar o canal do som tocando, se necessário
+
+local somAudio = audio.loadSound("audios/audio3.mp3")
  
 function scene:create( event )
     local sceneGroup = self.view
@@ -27,11 +32,7 @@ function scene:create( event )
     local botaoanterior = display.newImage(sceneGroup, "BOTOES/anterior.png")
     botaoanterior.x = display.contentWidth - botaoanterior.width/2 - MARGIN - 525
     botaoanterior.y = display.contentHeight - botaoanterior.height/2 - MARGIN +10
-
-    local som = display.newImage(sceneGroup, "BOTOES/somligado.png")
-    som.x = display.contentWidth - som.width / 2 - MARGIN -470
-    som.y = display.contentHeight - som.height / 2 - MARGIN -770  
-
+    
     botaoanterior:addEventListener("tap", function (event)
         composer.gotoScene("tela2", {
             effect = "fade",
@@ -40,6 +41,39 @@ function scene:create( event )
 
  
     end)
+    -- Função para alternar som ligado/desligado
+    local som = display.newImage(sceneGroup, "BOTOES/somdesligado.png")
+    som.x = display.contentWidth - som.width / 2 - MARGIN - 470
+    som.y = display.contentHeight - som.height / 2 - MARGIN - 775
+
+-- Função para alternar som ligado/desligado
+    local somLigado = false
+    local function toggleSom(event)
+        if somLigado then
+            somLigado = false
+            som.fill = {type = "image", filename = "BOTOES/somdesligado.png"}
+            som.x = display.contentWidth - som.width / 2 - MARGIN - 470
+            som.y = display.contentHeight - som.height / 2 - MARGIN - 756
+        -- Parar o som
+            if somAtivo then
+                audio.stop(somAtivo)
+                somAtivo = nil
+            end
+            print("Som desligado")
+        else
+            somLigado = true
+            som.fill = {type = "image", filename = "BOTOES/somligado.png"} 
+            som.x = display.contentWidth - som.width / 2 - MARGIN - 470
+            som.y = display.contentHeight - som.height / 2 - MARGIN - 770          
+        
+        
+            somAtivo = audio.play(somAudio, {loops = 0}) -- loops = 0 garante que o som toque apenas uma vez
+            print("Som ligado")
+        end
+        return true
+    end
+
+    som:addEventListener("tap", toggleSom)
     
 end
  
@@ -60,6 +94,10 @@ function scene:hide( event )
     local phase = event.phase
  
     if ( phase == "will" ) then
+        if somAtivo then
+            audio.stop(somAtivo)
+            somAtivo = nil
+        end
     elseif ( phase == "did" ) then
  
     end
@@ -68,6 +106,10 @@ end
  
 function scene:destroy( event )
     local sceneGroup = self.view
+    if somAudio then
+        audio.dispose(somAudio)
+        somAudio = nil
+    end
 end
  
 scene:addEventListener( "create", scene )
